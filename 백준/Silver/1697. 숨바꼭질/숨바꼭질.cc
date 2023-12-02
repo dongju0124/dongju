4e2bb549397e3e53310a5a7b95ff3c06ce8d8078
dongju0124 <54874474+dongju0124@users.noreply.github.com>
2023-12-02 22:26:32 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2252 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<queue>
+
+using namespace std;
+
+int N, K;
+bool visited[100001];
+
+int main()
+{
+	cin >> N >> K;
+
+	queue<pair<int, int>> q;
+	q.push(make_pair( N,0 ));
+	visited[N] = true;
+
+	while (!q.empty())
+	{
+		int now = q.front().first;
+		int count = q.front().second;
+		q.pop();
+
+		if (now == K)
+		{
+			cout << count;
+			break;
+		}
+
+		if (now - 1 >=0 && !visited[now - 1])
+		{
+			q.push(make_pair(now-1, count + 1 ));
+			visited[now - 1]= true;
+		}		
+		
+		if (now + 1 <= 100000 && !visited[now + 1])
+		{
+			q.push(make_pair(now + 1, count + 1 ));
+			visited[now + 1]= true;
+		}
+
+		if (now * 2 <= 100000 && !visited[now * 2]) {
+			q.push(make_pair(now * 2, count + 1 ));
+			visited[now * 2] = true;
+		}
+
+
+	}
+	
+}
