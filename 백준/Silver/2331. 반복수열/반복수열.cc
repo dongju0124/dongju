75b61a844fe71c2af061812e4b05cfe873f3d7b1
dongju0124 <54874474+dongju0124@users.noreply.github.com>
2024-01-16 18:48:45 +0900

    [Silver IV] Title: 반복수열, Time: 0 ms, Memory: 4284 KB -BaekjoonHub

---
+++ 백준/Silver/2331. 반복수열/반복수열.cc
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<math.h>
+#include<string>
+
+using namespace std;
+
+string A;
+int P;
+
+int num[7];
+int dp[236196];
+int order[236196];
+bool visited[236196];
+
+void find_num(int cnt, int N)
+{
+	int next_N = 0;
+	if (visited[N] == true )
+	{
+		cout << order[N]-1;
+		return;
+	}
+	order[N] = cnt;
+
+	visited[N] = true;
+
+	while (N)
+	{
+		next_N += pow(N % 10, P);;
+		N /= 10;
+	}
+
+
+	find_num(++cnt, next_N);
+
+}
+
+int main() {
+
+
+	cin >> A >> P;
+
+	dp[1] = stoi(A);
+
+	find_num(1, dp[1]);
+}
