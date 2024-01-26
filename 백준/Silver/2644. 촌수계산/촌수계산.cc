53540e4e5bbf5e26dbde0951f2ebb5d2b1cf1453
dongju0124 <54874474+dongju0124@users.noreply.github.com>
2024-01-26 23:56:35 +0900

    [Silver II] Title: 촌수계산, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.cc
+++ 백준/Silver/2644. 촌수계산/촌수계산.cc
@@ -1,36 +1,25 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 
 using namespace std;
 
-
 int graph[101][101]; // 관계를 나타내는 그래프
 int visited[101];           // 방문 여부를 나타내는 배열
+int result = -1;                  // 최종 결과
 
-int bfs(int start, int end, int n) {
-    queue<pair<int, int>> q; // 현재 노드와 촌수를 저장하는 큐
-    q.push({ start, 0 });      // 시작 노드와 촌수 0을 큐에 삽입
-    visited[start] = 1;      // 시작 노드 방문 표시
-
-    while (!q.empty()) {
-        int current = q.front().first;
-        int cnt = q.front().second;
-        q.pop();
-
-        if (current == end) {
-            return cnt; // 목표 노드에 도달한 경우 촌수 반환
-        }
-
-        for (int i = 1; i <= n; ++i) {
-            if (graph[current][i] && !visited[i]) {
-                q.push({ i, cnt + 1 });
-                visited[i] = 1; // 방문 표시
-            }
-        }
+void dfs(int current, int end, int depth) {
+    if (current == end) {
+        result = depth; // 목표 노드에 도달한 경우 촌수 업데이트
+        return;
     }
 
-    return -1; // 목표 노드에 도달하지 못한 경우 -1 반환
+    for (int i = 1; i <= 101; ++i) {
+        if (graph[current][i] && !visited[i]) {
+            visited[i] = 1; // 방문 표시
+            dfs(i, end, depth + 1);
+            visited[i] = 0; // 백트래킹
+        }
+    }
 }
 
 int main() {
@@ -44,7 +33,8 @@ int main() {
         graph[child][parent] = 1; // 양방향 그래프로 표현
     }
 
-    int result = bfs(target1, target2, n);
+    visited[target1] = 1; // 시작 노드 방문 표시
+    dfs(target1, target2, 0);
 
     cout << result << endl;
 
