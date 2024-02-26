17d2b10ff8241f118e62140e660a517f4294fe04
dongju0124 <54874474+dongju0124@users.noreply.github.com>
2024-02-26 12:51:34 +0900

    [level 2] Title: 3 x n 타일링, Time: 0.89 ms, Memory: 3.91 MB -BaekjoonHub

---
+++ 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <math.h>
+
+using namespace std;
+
+long long dp[5001];
+
+int solution(int n) {
+    
+    int answer = 0;
+	dp[0] = 1;
+	dp[2] = 3;
+
+	for (int i = 4; i <= n; i+=2)
+	{
+		dp[i] = dp[i - 2] * 3;
+
+		for (int j = i-4; j >= 0; j-=2)
+		{
+			dp[i] = dp[i] + (dp[j] * 2);
+		}
+        dp[i] = dp[i]%1000000007;
+	}
+    
+    answer = dp[n];
+    return answer;
+}
