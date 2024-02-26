b1cceba773e9956a75a5309142c0f023d66cac44
dongju0124 <54874474+dongju0124@users.noreply.github.com>
2024-02-26 12:46:53 +0900

    [Gold IV] Title: 타일 채우기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.cc
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;	
+int dp[100];
+int main() {
+
+	int n;
+
+	cin >> n;
+
+	dp[0] = 1;
+	dp[2] = 3;
+
+	for (int i = 4; i <= n; i+=2)
+	{
+		dp[i] = dp[i - 2] * 3;
+
+		for (int j = i-4; j >= 0; j-=2)
+		{
+			dp[i] = dp[i] + (dp[j] * 2);
+		}
+	}
+
+	cout << dp[n];
+}
