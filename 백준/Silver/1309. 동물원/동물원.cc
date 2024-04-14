67312391b350d12e635aebcd4e2d150e4a348635
dongju0124 <54874474+dongju0124@users.noreply.github.com>
2024-04-14 20:17:20 +0900

    [Silver I] Title: 동물원, Time: 0 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/1309. 동물원/동물원.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int dp[100001];
+
+int main() {
+
+    dp[0] = 1;
+    dp[1] = 3;
+    int N;
+    cin >> N;
+    for (int i = 2; i <= N; i++)
+        dp[i] = (dp[i - 1] * 2 + dp[i - 2]) % 9901;
+    cout << dp[N];
+}
