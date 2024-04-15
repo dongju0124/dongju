92487a3fce1e8c18f01c6e6f5b6843f5ff25b675
dongju0124 <54874474+dongju0124@users.noreply.github.com>
2024-04-15 22:06:52 +0900

    [level 3] Title: 입국심사, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int n, vector<int> times) {
+    long long answer = 0;
+    sort(times.begin(), times.end());
+
+    long long left = 1;
+    long long right = n * (long long)times.back();
+
+    while (left <= right) {
+        long long mid = (left + right) / 2;
+        long long temp = 0;
+
+        for (int i = 0; i < times.size(); i++) {
+            temp += (mid / (long long)times[i]);
+        }
+
+        if (temp >= n) {
+            right = mid - 1;
+            answer = mid;
+        }
+        else left = mid + 1;
+    }
+    return answer;
+}
