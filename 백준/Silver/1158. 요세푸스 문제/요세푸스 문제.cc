e650e0b22c0c761b3c8965312acd5180b9ae5693
dongju0124 <54874474+dongju0124@users.noreply.github.com>
2024-06-14 22:38:14 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 68 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int N, K;
+
+queue<int> q;
+
+int main()
+{
+	cin >> N >> K;
+
+	for (int i = 1; i <= N; i++)
+		q.push(i);
+
+	cout << "<";
+
+	while (q.size()>1)
+	{
+
+		for (int i = 1; i < K; i++)
+		{
+			int temp = q.front();
+			q.pop();
+			q.push(temp);
+		}
+
+		cout << q.front() << ", ";
+		q.pop();
+	}
+
+	cout << q.front() << ">\n";
+}
