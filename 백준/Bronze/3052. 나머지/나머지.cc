e85c7acb440e8ea0990478896fc6f3511236958c
dongju0124 <54874474+dongju0124@users.noreply.github.com>
2025-04-10 23:33:56 +0900

    [Bronze II] Title: 나머지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.cc
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int arr[42];
+
+int main()
+{
+	int n;
+	int cnt = 0;
+	for (int i = 0; i < 10; i++)
+	{
+		cin >> n;
+
+		arr[n % 42]++;
+	}
+
+	for (int i = 0; i < 42; i++)
+	{
+		if (arr[i] != 0)
+			cnt++;
+	}
+
+	cout << cnt;
+}
