96fc49d4b18c38ca218d83dc425e7d3998c4d9ef
dongju0124 <54874474+dongju0124@users.noreply.github.com>
2025-08-16 22:44:21 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int N;
+
+int main() {
+
+	cin >> N;
+	for (int i = 1; i <= N; i++) {
+
+		for (int j = 1; j <= N - i; j++) {
+			cout << " ";
+		}
+		for (int j = 1; j <= i; j++) {
+			cout << "*";
+		}
+		cout << endl;
+	}
+	return 0;
+
+}
